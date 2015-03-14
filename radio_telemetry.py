0b2df340957d50accf7bf502caccf398c6c4c461
pcgoddard <pagegoddard@ucla.edu>
2015-03-13 22:52:44 -0700

    Add code to extract resting HR data

--- radio_telemetry.py
+++ radio_telemetry.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env ipython
 
-##parse out time and date##
+##PARSE TIME AND DATE##
 def replace_all(text, dic):
     for i, j in dic.iteritems():
         text = text.replace(i, j)
     return text
-###REPLACE 'FILE' WITH EXACT FILE NAME###
+#replace 'FILE' WITH EXACT FILE NAME###
 f1 = open('C2Q175P1_7_abcefgh.csv').read() #opens original csv file as string 'f1'
 f2 = open('parsed_C2Q175P1_7_abcefgh.csv', 'w') #creates new file to write the parsed data to via file object 'f2'
 reps = {' ':',', '/':'_', 'RealTime':'Date', 'Event':'Realtime'} 
@@ -16,8 +16,8 @@ f1parsed = replace_all(f1, reps) #creates string with edited f1 data
 f2.write(f1parsed) #writes parsed data to file stored in f2
 f2.close() #closes file object
 
-##noise filtering on temperature data##
-	#in column 10 (K), if the difference between numerical lines is less than -0.2 or greater than 0.2, remove line
+##FILTER TEMPERATURE DATA##
+#in column 10 (K), if the difference between numerical lines is less than -0.2 or greater than 0.2, remove line
 f2 = open("parsed_C2Q175P1_7_abcefgh.csv")
 f3 = open("filtered_parsed_C2Q175P1_7_abcefgh.csv", "w")
 f3.write("ElapsedTime,Date,Realtime, , ,I1Num,I1RR-I,I1RR-I(SD),I1HR,I1HR(SD),I2T_Mean,I2T_Mean(SD),I3A_TA,I3A_TA(SD)\r\n") #adds corrected header
@@ -33,9 +33,9 @@ for line in f2:
 f2.close()
 f3.close()
 
-##conditional to select heart rate data at certain activity levels##
-#active HR: extract lines with activity between 3 and 4 units (row[12])
-f3 = open("filtered_parsed_FILE.csv")
+##CONDITIONAL: EXTRACT DATA AT CONTOLLED ACTIVITY LEVELS##
+#active heartrate: extract lines with activity between 3 and 4 units (row[12])
+f3 = open("filtered_parsed_FILE.csv") 
 f3.readline()
 f4 = open("activeHR_FILE.csv", "w")
 f4.write("ElapsedTime,Date,Realtime, , ,I1Num,I1RR-I,I1RR-I(SD),I1HR,I1HR(SD),I2T_Mean,I2T_Mean(SD),I3A_TA,I3A_TA(SD)\r\n")
@@ -45,7 +45,24 @@ for line in f3:
         f5.write(line)
 f3.close()
 f4.close()
-#resting HR: extract lines with activity = 0 AND following 3 lines of activity = 0
+
+#resting heartrate: extract lines with activity = 0 following at least one full minute of rest
+f3 = open("filtered_parsed_FILE.csv")
+f3.readline()
+f5 = open("restHR_FILE.csv", "w")
+f5.write("ElapsedTime,Date,Realtime, , ,I1Num,I1RR-I,I1RR-I(SD),I1HR,I1HR(SD),I2T_Mean,I2T_Mean(SD),I3A_TA,I3A_TA(SD)\r\n")
+counts = 0
+for line in f3:
+    fields = line.split(",")
+    if float(fields[12]) == 0:
+        counts = counts + 1 #if mouse has no activity, add to counts
+    else:
+        counts == 0 #if there is activity, reset counter to 0
+    if counts > 2 and float(fields[12]) == 0:
+        f5.write(line) #if counts is greater than 2, there are at least 3 preceding lines (1 minute) of no activity; write the current line to resting HR document
+f3.close() #close files
+f5.close()
+
 
 #Graphing in R
 	#talk to Tamara
